60a33e98a1d6ba358ae048122fd2f853adf287b8
sukanyachand <sukanya9791@gmail.com>
2016-08-06 20:15:10 +0530

    Session1 Assignment2
    Session1 Assignment2

---
+++ VariableDeclaration.java
@@ -0,0 +1,13 @@
+
+package sumnumber;
+
+public class VariableDeclaration {
+
+    public static void main(String[] args) {
+    //This is to declare three integer variables a,b,c and initialize b to 4
+    //Session 1 assignment 2
+    int a, b = 4,c;
+    System.out.println("Value of b:" +b);
+    }
+    
+}
